f95a1c657c9d8ad8896f20e47fd609968b34a652
JohnCorby <williamforgames6@gmail.com>
2023-06-07 16:14:48 -0700

    Initial commit

---
+++ SlipNSlide/Mod.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using OWML.Common;
+using OWML.ModHelper;
+using System.Reflection;
+
+namespace SlipNSlide;
+
+[HarmonyPatch]
+public class Mod : ModBehaviour
+{
+	public Mod Instance;
+
+	private void Awake()
+	{
+		Instance = this;
+		Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+	}
+
+	private void Start()
+	{
+		var sound = ModHelper.Assets.GetAudio("sound.mp3");
+		// Locator.GetAudioManager().
+	}
+}
