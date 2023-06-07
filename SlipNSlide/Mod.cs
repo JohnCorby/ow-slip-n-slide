f73ba99ab79f1a4aba928c0c59bf2217c4f869bd
JohnCorby <williamforgames6@gmail.com>
2023-06-07 16:53:41 -0700

    goobing

--- SlipNSlide/Mod.cs
+++ SlipNSlide/Mod.cs
@@ -1,32 +1,43 @@
 ﻿using HarmonyLib;
 using OWML.ModHelper;
-using System.Reflection;
+using System.Linq;
+using UnityEngine;
 
 namespace SlipNSlide;
 
 [HarmonyPatch]
 public class Mod : ModBehaviour
 {
-	public Mod Instance;
-
-	private void Awake()
-	{
-		Instance = this;
-		Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
-	}
-
 	private void Start()
 	{
-		return;
-		Locator.GetAudioManager()._audioLibraryDict[(int)AudioType.MovementIceLSiding] =
-			new AudioLibrary.AudioEntry(AudioType.MovementIceLSiding, new[] { ModHelper.Assets.GetAudio("sound.mp3") });
-	}
+		LoadManager.OnCompleteSceneLoad += (_, _) =>
+		{
+			var sound = ModHelper.Assets.GetAudio("sound.mp3");
 
-	[HarmonyPrefix, HarmonyPatch(typeof(PlayerCharacterController), nameof(PlayerCharacterController.IsSlidingOnIce))]
-	private static bool PlayerCharacterController_IsSlidingOnIce(PlayerCharacterController __instance, out bool __result)
-	{
-		__result = __instance.IsGrounded() && __instance._groundCollider.material.dynamicFriction == 0f;
-		// __result = __instance.IsGrounded() && __instance._groundSurface == SurfaceType.Ice && __instance._groundCollider.material.dynamicFriction == 0f;
-		return false;
+			ModHelper.Events.Unity.FireInNUpdates(() =>
+			{
+				// friction
+				foreach (var collider in Resources.FindObjectsOfTypeAll<Collider>())
+				{
+					collider.material.dynamicFriction = 0;
+				}
+
+				// surface type
+				var surfaceTable = Locator.GetSurfaceManager()._lookupTable;
+				foreach (var kvp in surfaceTable.ToList())
+				{
+					surfaceTable[kvp.Key] = SurfaceType.Ice;
+				}
+
+				// audio
+				var audioTable = Locator.GetAudioManager()._audioLibraryDict;
+				foreach (var kvp in audioTable.ToList())
+				{
+					audioTable[kvp.Key] = new AudioLibrary.AudioEntry((AudioType)kvp.Key, new[] { sound });
+				}
+
+				ModHelper.Console.WriteLine("everything is ice");
+			}, 10);
+		};
 	}
 }
