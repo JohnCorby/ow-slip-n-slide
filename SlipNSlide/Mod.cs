a6db8af879aec404eb2fd2e817edf0489b9dc3f9
JohnCorby <williamforgames6@gmail.com>
2023-06-07 17:12:17 -0700

    so silly

--- SlipNSlide/Mod.cs
+++ SlipNSlide/Mod.cs
@@ -12,7 +12,10 @@ public class Mod : ModBehaviour
 	{
 		LoadManager.OnCompleteSceneLoad += (_, _) =>
 		{
-			var sound = ModHelper.Assets.GetAudio("sound.mp3");
+			var slide = ModHelper.Assets.GetAudio("slide.mp3");
+			var slide2 = ModHelper.Assets.GetAudio("slide2.mp3");
+			var bonk = ModHelper.Assets.GetAudio("bonk.mp3");
+			var music = ModHelper.Assets.GetAudio("music.mp3");
 
 			ModHelper.Events.Unity.FireInNUpdates(() =>
 			{
@@ -31,10 +34,18 @@ public class Mod : ModBehaviour
 
 				// audio
 				var audioTable = Locator.GetAudioManager()._audioLibraryDict;
-				foreach (var kvp in audioTable.ToList())
-				{
-					audioTable[kvp.Key] = new AudioLibrary.AudioEntry((AudioType)kvp.Key, new[] { sound });
-				}
+				audioTable[(int)AudioType.MovementIceLSiding] = new AudioLibrary.AudioEntry(AudioType.MovementIceLSiding, new[] { slide, slide2 });
+
+				audioTable[(int)AudioType.ImpactUnderwater] = new AudioLibrary.AudioEntry(AudioType.ImpactUnderwater, new[] { bonk });
+				audioTable[(int)AudioType.ImpactLowSpeed] = new AudioLibrary.AudioEntry(AudioType.ImpactLowSpeed, new[] { bonk });
+				audioTable[(int)AudioType.ImpactMediumSpeed] = new AudioLibrary.AudioEntry(AudioType.ImpactMediumSpeed, new[] { bonk });
+				audioTable[(int)AudioType.ImpactHighSpeed] = new AudioLibrary.AudioEntry(AudioType.ImpactHighSpeed, new[] { bonk });
+				audioTable[(int)AudioType.ShipImpact_NoDamage] = new AudioLibrary.AudioEntry(AudioType.ShipImpact_NoDamage, new[] { bonk });
+				audioTable[(int)AudioType.ShipImpact_LightDamage] = new AudioLibrary.AudioEntry(AudioType.ShipImpact_LightDamage, new[] { bonk });
+				audioTable[(int)AudioType.ShipImpact_MediumDamage] = new AudioLibrary.AudioEntry(AudioType.ShipImpact_MediumDamage, new[] { bonk });
+				audioTable[(int)AudioType.ShipImpact_HeavyDamage] = new AudioLibrary.AudioEntry(AudioType.ShipImpact_HeavyDamage, new[] { bonk });
+
+				audioTable[(int)AudioType.NomaiRuinsBaseTrack] = new AudioLibrary.AudioEntry(AudioType.NomaiRuinsBaseTrack, new[] { music });
 
 				ModHelper.Console.WriteLine("everything is ice");
 			}, 10);
