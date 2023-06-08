0fcb03053c42ee18bae87a443bcd5523002b43db
JohnCorby <williamforgames6@gmail.com>
2023-06-07 18:20:05 -0700

    impact

--- SlipNSlide/Mod.cs
+++ SlipNSlide/Mod.cs
@@ -38,13 +38,25 @@ public class Mod : ModBehaviour
 				audioTable[(int)AudioType.MovementIceLSiding] = new AudioLibrary.AudioEntry(AudioType.MovementIceLSiding, new[] { slide, slide2 });
 
 				audioTable[(int)AudioType.ImpactUnderwater] = new AudioLibrary.AudioEntry(AudioType.ImpactUnderwater, new[] { bonk });
+				audioTable[(int)AudioType.ImpactHighSpeed] = new AudioLibrary.AudioEntry(AudioType.ImpactHighSpeed, new[] { bonk });
 				audioTable[(int)AudioType.ImpactLowSpeed] = new AudioLibrary.AudioEntry(AudioType.ImpactLowSpeed, new[] { bonk });
 				audioTable[(int)AudioType.ImpactMediumSpeed] = new AudioLibrary.AudioEntry(AudioType.ImpactMediumSpeed, new[] { bonk });
-				audioTable[(int)AudioType.ImpactHighSpeed] = new AudioLibrary.AudioEntry(AudioType.ImpactHighSpeed, new[] { bonk });
-				audioTable[(int)AudioType.ShipImpact_NoDamage] = new AudioLibrary.AudioEntry(AudioType.ShipImpact_NoDamage, new[] { bonk });
+				audioTable[(int)AudioType.DefaultPropImpact] = new AudioLibrary.AudioEntry(AudioType.DefaultPropImpact, new[] { bonk });
+				audioTable[(int)AudioType.ModelShipImpact] = new AudioLibrary.AudioEntry(AudioType.ModelShipImpact, new[] { bonk });
+				audioTable[(int)AudioType.NomaiShuttleImpact] = new AudioLibrary.AudioEntry(AudioType.NomaiShuttleImpact, new[] { bonk });
+				audioTable[(int)AudioType.BH_MeteorImpact] = new AudioLibrary.AudioEntry(AudioType.BH_MeteorImpact, new[] { bonk });
+				audioTable[(int)AudioType.DB_VineImpact] = new AudioLibrary.AudioEntry(AudioType.DB_VineImpact, new[] { bonk });
+				audioTable[(int)AudioType.TH_PickaxeImpact] = new AudioLibrary.AudioEntry(AudioType.TH_PickaxeImpact, new[] { bonk });
+				audioTable[(int)AudioType.Raft_Impact_Heavy] = new AudioLibrary.AudioEntry(AudioType.Raft_Impact_Heavy, new[] { bonk });
+				audioTable[(int)AudioType.Raft_Impact_Light] = new AudioLibrary.AudioEntry(AudioType.Raft_Impact_Light, new[] { bonk });
+				audioTable[(int)AudioType.Raft_Impact_Medium] = new AudioLibrary.AudioEntry(AudioType.Raft_Impact_Medium, new[] { bonk });
+				audioTable[(int)AudioType.Raft_Impact_Player] = new AudioLibrary.AudioEntry(AudioType.Raft_Impact_Player, new[] { bonk });
+				audioTable[(int)AudioType.WoodImpact_Large] = new AudioLibrary.AudioEntry(AudioType.WoodImpact_Large, new[] { bonk });
+				audioTable[(int)AudioType.WoodImpact_Small] = new AudioLibrary.AudioEntry(AudioType.WoodImpact_Small, new[] { bonk });
+				audioTable[(int)AudioType.ShipImpact_HeavyDamage] = new AudioLibrary.AudioEntry(AudioType.ShipImpact_HeavyDamage, new[] { bonk });
 				audioTable[(int)AudioType.ShipImpact_LightDamage] = new AudioLibrary.AudioEntry(AudioType.ShipImpact_LightDamage, new[] { bonk });
 				audioTable[(int)AudioType.ShipImpact_MediumDamage] = new AudioLibrary.AudioEntry(AudioType.ShipImpact_MediumDamage, new[] { bonk });
-				audioTable[(int)AudioType.ShipImpact_HeavyDamage] = new AudioLibrary.AudioEntry(AudioType.ShipImpact_HeavyDamage, new[] { bonk });
+				audioTable[(int)AudioType.ShipImpact_NoDamage] = new AudioLibrary.AudioEntry(AudioType.ShipImpact_NoDamage, new[] { bonk });
 
 				audioTable[(int)AudioType.NomaiRuinsBaseTrack] = new AudioLibrary.AudioEntry(AudioType.NomaiRuinsBaseTrack, new[] { music });
 				audioTable[(int)AudioType.DreamRuinsBaseTrack] = new AudioLibrary.AudioEntry(AudioType.DreamRuinsBaseTrack, new[] { music });
